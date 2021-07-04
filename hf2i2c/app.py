b1f84a16e5c0f394a18d46d5fc71e293bea19d6c
Michael Hope <mlhx@google.com>
2021-07-04 14:46:37 +0200

    zx0: ran addlicense
    Update the source to include the full license block.

--- hf2i2c/app.py
+++ hf2i2c/app.py
@@ -1,3 +1,17 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Host side programmer for the ZX0 bootloader.
 
 The bootloader implements the HF2 protocol. See https://github.com/microsoft/uf2/blob/master/hf2.md for the definition including the protocol level values.
